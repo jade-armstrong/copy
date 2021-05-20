3d33b354c969004db9ebfbb8a70694c70cba3264
jade-armstrong <jade@wethinkcode.co.za>
2021-05-20 09:24:13 +0200

    Edited comments

--- copy.sh
+++ copy.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-REPOSSH=<repo-SSH>
-REPODIR=<repo-directory>
-REPO=$REPODIR<repository>
-FILE=$REPO<the-file-to-copy>
-DIR=<directory-to-copy-to>
+#This is a simple bash script that will copy a file from a git repository on your machine to another directory.
+
+REPOSSH= #Enter the repo SSH key here for cloning the repo that holds the file you want to copy
+REPODIR= #Enter the directory path you have or want the repo to be under e.g. ~/repos
+REPO=$REPODIR/ #Enter the repo name here
+FILE=$REPO/ #Enter the rest of the path from the repo of the file you want to copy
+DIR= #Enter the path to the directory you want to copy the file to e.g. ~/repos/myrepo
 
 [[ ! -d $REPODIR ]] || mkdir $REPODIR
 [[ ! -d $REPO ]] || git -C $REPODIR clone $REPOSSH
